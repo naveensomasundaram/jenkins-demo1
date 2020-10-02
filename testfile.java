979617805a83dbf696c133338124798aa6799662
Naveen <naveenprasadsomasundaram@gmail.com>
2020-10-02 17:23:54 +0530

    checking in the java file

---
+++ testfile.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+
+public class testfile {
+
+    public static void main(String[] args) {
+        LocalDateTime current = LocalDateTime.now();
+
+        System.out.println("Current Date and Time is: " + current);
+    }
+}
